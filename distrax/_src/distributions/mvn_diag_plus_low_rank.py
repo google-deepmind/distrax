f1d02838f7a771f16238f7db4372f3c893d1cf49
Francisco Ruiz <franrruiz@google.com>
2022-03-14 05:06:30 -0700

    Implement the `MultivariateNormalDiagPlusLowRank` distribution.
    PiperOrigin-RevId: 434433963

---
+++ distrax/_src/distributions/mvn_diag_plus_low_rank.py
@@ -0,0 +1,212 @@
+# Copyright 2021 DeepMind Technologies Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""MultivariateNormalDiagPlusLowRank distribution."""
+
+from typing import Optional
+
+import chex
+from distrax._src.bijectors.diag_affine import DiagAffine
+from distrax._src.bijectors.diag_plus_low_rank_affine import DiagPlusLowRankAffine
+from distrax._src.distributions import distribution
+from distrax._src.distributions.mvn_from_bijector import MultivariateNormalFromBijector
+from distrax._src.utils import conversion
+import jax
+import jax.numpy as jnp
+from tensorflow_probability.substrates import jax as tfp
+
+tfd = tfp.distributions
+
+Array = chex.Array
+
+
+def _check_parameters(
+    loc: Optional[Array],
+    scale_diag: Optional[Array],
+    scale_u_matrix: Optional[Array],
+    scale_v_matrix: Optional[Array]) -> None:
+  """Checks that the inputs are correct."""
+
+  if all(x is None for x in [loc, scale_diag, scale_u_matrix]):
+    raise ValueError(
+        'At least one of `loc`, `scale_diag`,  and `scale_u_matrix` must '
+        'be specified.')
+  if scale_v_matrix is not None and scale_u_matrix is None:
+    raise ValueError('`scale_v_matrix` can be specified only when '
+                     '`scale_u_matrix` is also specified.')
+
+  for name, x, n_dims in [
+      ('loc', loc, 1), ('scale_diag', scale_diag, 1),
+      ('scale_u_matrix', scale_u_matrix, 2),
+      ('scale_v_matrix', scale_v_matrix, 2)]:
+    if x is not None and x.ndim < n_dims:
+      raise ValueError(f'`{name}` must have at least {n_dims} dimensions.')
+
+  if scale_u_matrix is not None and scale_v_matrix is not None:
+    if scale_u_matrix.shape[-1] != scale_v_matrix.shape[-1]:
+      raise ValueError(
+          f'The last dimension of `scale_u_matrix` must coincide with '
+          f'the last dimension of `scale_v_matrix`, but got '
+          f'`scale_u_matrix.shape[-1] = {scale_u_matrix.shape[-1]}`'
+          f' and `scale_v_matrix.shape[-1] = {scale_v_matrix.shape[-1]}`.')
+
+  if scale_u_matrix is not None and scale_u_matrix.shape[-1] < 1:
+    raise ValueError(
+        'The last dimension of `scale_u_matrix` cannot be zero.')
+
+  loc_dim = None if loc is None else loc.shape[-1]
+  scale_diag_dim = None if scale_diag is None else scale_diag.shape[-1]
+  scale_u_matrix_dim = (
+      None if scale_u_matrix is None else scale_u_matrix.shape[-2])
+  scale_v_matrix_dim = (
+      None if scale_v_matrix is None else scale_v_matrix.shape[-2])
+  num_dims = loc_dim if loc_dim is not None else scale_diag_dim
+  num_dims = num_dims if num_dims is not None else scale_u_matrix_dim
+  array_dims = [
+      x for x in [
+          loc_dim, scale_diag_dim, scale_u_matrix_dim, scale_v_matrix_dim]
+      if x is not None]
+  if not all(x == num_dims for x in array_dims):
+    raise ValueError(
+        f'If specified, the following shapes must all coincide, but got '
+        f'`loc.shape[-1] = {loc_dim}`, '
+        f'`scale_diag.shape[-1] = {scale_diag_dim}`, '
+        f'`scale_u_matrix.shape[-2] = {scale_u_matrix_dim}`, and '
+        f'`scale_v_matrix.shape[-2] = {scale_v_matrix_dim}`.')
+
+
+class MultivariateNormalDiagPlusLowRank(MultivariateNormalFromBijector):
+  """Multivariate normal distribution on `R^k`.
+
+  The `MultivariateNormalDiagPlusLowRank` distribution is parameterized by a
+  location (mean) vector `b` and a scale matrix `S` that has the following
+  structure: `S = diag(D) + U @ V.T`, where `D` is a `k`-length vector, and both
+  `U` and `V` are `k x r` matrices (with `r < k` typically). The covariance
+  matrix of the multivariate normal distribution is `C = S @ S.T`.
+
+  This class makes no attempt to verify that the scale matrix `S` is invertible,
+  which happens if and only if both `diag(D)` and `I + V^T diag(D)^{-1} U` are
+  invertible. It is the responsibility of the user to make sure that this is the
+  case.
+  """
+
+  equiv_tfp_cls = tfd.MultivariateNormalDiagPlusLowRank
+
+  def __init__(self,
+               loc: Optional[Array] = None,
+               scale_diag: Optional[Array] = None,
+               scale_u_matrix: Optional[Array] = None,
+               scale_v_matrix: Optional[Array] = None):
+    """Initializes a MultivariateNormalDiagPlusLowRank distribution.
+
+    Args:
+      loc: Mean vector of the distribution of shape `k` (can also be a batch of
+        such vectors). If not specified, it defaults to zeros.
+      scale_diag: The diagonal matrix added to the scale `S`, specified by a
+        `k`-length vector containing its diagonal entries (or a batch of
+        vectors). If not specified, the diagonal matrix defaults to the
+        identity.
+      scale_u_matrix: The low-rank matrix `U` that specifies the scale, as
+        described in the class docstring. It is a `k x r` matrix (or a batch of
+        such matrices). If not specified, it defaults to zeros. At least one
+        of `loc`, `scale_diag`, and `scale_u_matrix` must be specified.
+      scale_v_matrix: The low-rank matrix `V` that specifies the scale, as
+        described in the class docstring. It is a `k x r` matrix (or a batch of
+        such matrices). If not specified, it defaults to `scale_u_matrix`. It
+        can only be specified if `scale_u_matrix` is also specified.
+    """
+    loc = None if loc is None else conversion.as_float_array(loc)
+    scale_diag = None if scale_diag is None else conversion.as_float_array(
+        scale_diag)
+    scale_u_matrix = (
+        None if scale_u_matrix is None else conversion.as_float_array(
+            scale_u_matrix))
+    scale_v_matrix = (
+        None if scale_v_matrix is None else conversion.as_float_array(
+            scale_v_matrix))
+
+    _check_parameters(loc, scale_diag, scale_u_matrix, scale_v_matrix)
+
+    if loc is not None:
+      num_dims = loc.shape[-1]
+    elif scale_diag is not None:
+      num_dims = scale_diag.shape[-1]
+    elif scale_u_matrix is not None:
+      num_dims = scale_u_matrix.shape[-2]
+
+    dtype = jnp.result_type(
+        *[x for x in [loc, scale_diag, scale_u_matrix, scale_v_matrix]
+          if x is not None])
+
+    if loc is None:
+      loc = jnp.zeros((num_dims,), dtype=dtype)
+
+    self._scale_diag = scale_diag
+    if scale_diag is None:
+      self._scale_diag = jnp.ones((num_dims,), dtype=dtype)
+
+    self._scale_u_matrix = scale_u_matrix
+    if scale_u_matrix is None:
+      self._scale_u_matrix = jnp.zeros((num_dims, 1), dtype=dtype)
+
+    self._scale_v_matrix = scale_v_matrix
+    if scale_v_matrix is None:
+      self._scale_v_matrix = self._scale_u_matrix
+
+    batch_shape = jax.lax.broadcast_shapes(
+        loc.shape[:-1], self._scale_diag.shape[:-1],
+        self._scale_u_matrix.shape[:-2], self._scale_v_matrix.shape[:-2])
+
+    if scale_u_matrix is None:
+      # The scale matrix is diagonal.
+      scale = DiagAffine(
+          diag=self._scale_diag,
+          bias=jnp.zeros(loc.shape[-1:], dtype=dtype))
+    else:
+      scale = DiagPlusLowRankAffine(
+          bias=jnp.zeros(loc.shape[-1:], dtype=dtype),
+          u_matrix=self._scale_u_matrix,
+          v_matrix=self._scale_v_matrix,
+          diag=self._scale_diag)
+    super().__init__(
+        loc=loc, scale=scale, batch_shape=batch_shape, dtype=dtype)
+
+  @property
+  def scale_diag(self) -> Array:
+    """Diagonal matrix that is added to the scale."""
+    return jnp.broadcast_to(
+        self._scale_diag, self.batch_shape + self.event_shape)
+
+  @property
+  def scale_u_matrix(self) -> Array:
+    """Matrix `U` that defines the low-rank part of the scale matrix."""
+    return jnp.broadcast_to(
+        self._scale_u_matrix,
+        self.batch_shape + self._scale_u_matrix.shape[-2:])
+
+  @property
+  def scale_v_matrix(self) -> Array:
+    """Matrix `V` that defines the low-rank part of the scale matrix."""
+    return jnp.broadcast_to(
+        self._scale_v_matrix,
+        self.batch_shape + self._scale_v_matrix.shape[-2:])
+
+  def __getitem__(self, index) -> 'MultivariateNormalDiagPlusLowRank':
+    """See `Distribution.__getitem__`."""
+    index = distribution.to_batch_shape_index(self.batch_shape, index)
+    return MultivariateNormalDiagPlusLowRank(
+        loc=self.loc[index],
+        scale_diag=self.scale_diag[index],
+        scale_u_matrix=self.scale_u_matrix[index],
+        scale_v_matrix=self.scale_v_matrix[index])
---
+++ distrax/_src/distributions/mvn_diag_plus_low_rank_test.py
@@ -0,0 +1,382 @@
+# Copyright 2021 DeepMind Technologies Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for `mvn_diag_plus_low_rank.py`."""
+
+from absl.testing import absltest
+from absl.testing import parameterized
+
+import chex
+from distrax._src.distributions.mvn_diag_plus_low_rank import MultivariateNormalDiagPlusLowRank
+from distrax._src.utils import equivalence
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+from tensorflow_probability.substrates import jax as tfp
+
+tfd = tfp.distributions
+
+
+def _covariance_matrix_from_low_rank(
+    scale_diag, scale_u_matrix, scale_v_matrix):
+  """Constructs the covariance matrix from the low-rank matrices."""
+  if scale_u_matrix is not None:
+    if scale_v_matrix is None:
+      scale_v_matrix = np.copy(scale_u_matrix)
+    scale_v_matrix_t = np.vectorize(
+        np.transpose, signature='(k,m)->(m,k)')(scale_v_matrix)
+    scale = np.matmul(scale_u_matrix, scale_v_matrix_t) + np.vectorize(
+        np.diag, signature='(k)->(k,k)')(scale_diag)
+  else:
+    scale = np.vectorize(np.diag, signature='(k)->(k,k)')(scale_diag)
+  scale_t = np.vectorize(np.transpose, signature='(k,k)->(k,k)')(scale)
+  return np.matmul(scale, scale_t)
+
+
+class MultivariateNormalDiagPlusLowRankTest(
+    equivalence.EquivalenceTest, parameterized.TestCase):
+
+  def setUp(self):
+    # pylint: disable=too-many-function-args
+    super().setUp(MultivariateNormalDiagPlusLowRank)
+
+  def assertion_fn(self, rtol):
+    return lambda x, y: np.testing.assert_allclose(x, y, rtol=rtol)
+
+  @parameterized.named_parameters(
+      ('all inputs are None', {}),
+      ('scale_v_matrix is provided but scale_u_matrix is None', {
+          'loc': np.zeros((4,)),
+          'scale_v_matrix': np.ones((4, 2)),
+      }),
+      ('wrong dimension of loc', {
+          'loc': np.array(0.),
+      }),
+      ('wrong dimension of scale_diag', {
+          'scale_diag': np.array(0.),
+      }),
+      ('wrong dimension of scale_u_matrix', {
+          'scale_u_matrix': np.ones((4,)),
+      }),
+      ('wrong dimension of scale_v_matrix', {
+          'scale_u_matrix': np.ones((4, 2)),
+          'scale_v_matrix': np.ones((4,)),
+      }),
+      ('last dimension of scale_u_matrix is zero', {
+          'scale_u_matrix': np.ones((4, 0)),
+      }),
+      ('inconsistent dimensions of scale_u_matrix and scale_v_matrix', {
+          'scale_u_matrix': np.ones((4, 2)),
+          'scale_v_matrix': np.ones((4, 1)),
+      }),
+      ('inconsistent event_dim across two params', {
+          'loc': np.zeros((4,)),
+          'scale_u_matrix': np.ones((5, 2)),
+      }),
+      ('inconsistent event_dim across three params', {
+          'loc': np.zeros((4,)),
+          'scale_diag': np.ones((5,)),
+          'scale_u_matrix': np.ones((4, 2)),
+      }),
+  )
+  def test_raises_on_wrong_inputs(self, dist_kwargs):
+    with self.assertRaises(ValueError):
+      MultivariateNormalDiagPlusLowRank(**dist_kwargs)
+
+  @parameterized.named_parameters(
+      ('loc provided', {'loc': np.zeros((4,))}),
+      ('scale_diag provided', {'scale_diag': np.ones((4,))}),
+      ('scale_u_matrix provided', {'scale_u_matrix': np.zeros((4, 1))}),
+  )
+  def test_default_properties(self, dist_kwargs):
+    dist = MultivariateNormalDiagPlusLowRank(**dist_kwargs)
+    self.assertion_fn(rtol=1e-3)(dist.loc, jnp.zeros((4,)))
+    self.assertion_fn(rtol=1e-3)(dist.scale_diag, jnp.ones((4,)))
+    self.assertion_fn(rtol=1e-3)(dist.scale_u_matrix, jnp.zeros((4, 1)))
+    self.assertion_fn(rtol=1e-3)(dist.scale_v_matrix, jnp.zeros((4, 1)))
+
+  @parameterized.named_parameters(
+      ('unbatched', (), (4,), (4,), (4, 2), (4, 2)),
+      ('batched loc', (7,), (7, 4), (4,), (4, 2), (4, 2)),
+      ('batched scale_diag', (7,), (4,), (7, 4), (4, 2), (4, 2)),
+      ('batched scale_u_matrix', (7,), (4,), (4,), (7, 4, 2), (4, 2)),
+      ('batched scale_v_matrix', (7,), (4,), (4,), (4, 2), (7, 4, 2)),
+  )
+  def test_properties(self, batch_shape, loc_shape, scale_diag_shape,
+                      scale_u_matrix_shape, scale_v_matrix_shape):
+    rng = np.random.default_rng(2022)
+    loc = rng.normal(size=loc_shape)
+    scale_diag = rng.normal(size=scale_diag_shape)
+    scale_u_matrix = rng.normal(size=scale_u_matrix_shape)
+    scale_v_matrix = rng.normal(size=scale_v_matrix_shape)
+    dist = MultivariateNormalDiagPlusLowRank(
+        loc=loc,
+        scale_diag=scale_diag,
+        scale_u_matrix=scale_u_matrix,
+        scale_v_matrix=scale_v_matrix,
+    )
+    self.assertEqual(dist.batch_shape, batch_shape)
+    self.assertion_fn(rtol=1e-3)(
+        dist.loc, jnp.broadcast_to(loc, batch_shape + (4,)))
+    self.assertion_fn(rtol=1e-3)(
+        dist.scale_diag, jnp.broadcast_to(scale_diag, batch_shape + (4,)))
+    self.assertion_fn(rtol=1e-3)(
+        dist.scale_u_matrix,
+        jnp.broadcast_to(scale_u_matrix, batch_shape + (4, 2)))
+    self.assertion_fn(rtol=1e-3)(
+        dist.scale_v_matrix,
+        jnp.broadcast_to(scale_v_matrix, batch_shape + (4, 2)))
+
+  @chex.all_variants
+  @parameterized.named_parameters(
+      ('unbatched, no shape', (), (4,), (4,), (4, 2), (4, 2)),
+      ('batched loc, no shape', (), (7, 4), (4,), (4, 2), (4, 2)),
+      ('batched scale_diag, no shape', (), (4,), (7, 4), (4, 2), (4, 2)),
+      ('batched scale_u_matrix, no shape', (), (4,), (4,), (7, 4, 2), (4, 2)),
+      ('batched scale_v_matrix, no shape', (), (4,), (4,), (4, 2), (7, 4, 2)),
+      ('unbatched, with shape', (3,), (4,), (4,), (4, 2), (4, 2)),
+      ('batched loc, with shape', (3,), (7, 4), (4,), (4, 2), (4, 2)),
+      ('batched scale_diag, with shape', (3,), (4,), (7, 4), (4, 2), (4, 2)),
+      ('batched scale_u_matrix, with shape',
+       (3,), (4,), (4,), (7, 4, 2), (4, 2)),
+      ('batched scale_v_matrix, with shape',
+       (3,), (4,), (4,), (4, 2), (7, 4, 2)),
+  )
+  def test_sample_shape(self, sample_shape, loc_shape, scale_diag_shape,
+                        scale_u_matrix_shape, scale_v_matrix_shape):
+    rng = np.random.default_rng(2022)
+    loc = rng.normal(size=loc_shape)
+    scale_diag = rng.normal(size=scale_diag_shape)
+    scale_u_matrix = rng.normal(size=scale_u_matrix_shape)
+    scale_v_matrix = rng.normal(size=scale_v_matrix_shape)
+    dist = MultivariateNormalDiagPlusLowRank(
+        loc=loc,
+        scale_diag=scale_diag,
+        scale_u_matrix=scale_u_matrix,
+        scale_v_matrix=scale_v_matrix,
+    )
+    tfp_dist = tfd.MultivariateNormalFullCovariance(
+        loc=loc,
+        covariance_matrix=_covariance_matrix_from_low_rank(
+            scale_diag, scale_u_matrix, scale_v_matrix)
+    )
+    sample_fn = self.variant(
+        lambda rng: dist.sample(sample_shape=sample_shape, seed=rng))
+    distrax_samples = sample_fn(jax.random.PRNGKey(0))
+    tfp_samples = tfp_dist.sample(
+        sample_shape=sample_shape, seed=jax.random.PRNGKey(0))
+    self.assertEqual(distrax_samples.shape, tfp_samples.shape)
+
+  @chex.all_variants
+  @parameterized.named_parameters(
+      ('float32', jnp.float32),
+      ('float64', jnp.float64))
+  def test_sample_dtype(self, dtype):
+    dist_params = {
+        'loc': np.array([0., 0.], dtype),
+        'scale_diag': np.array([1., 1.], dtype)}
+    dist = MultivariateNormalDiagPlusLowRank(**dist_params)
+    samples = self.variant(dist.sample)(seed=jax.random.PRNGKey(0))
+    self.assertEqual(samples.dtype, dist.dtype)
+    chex.assert_type(samples, dtype)
+
+  @chex.all_variants
+  @parameterized.named_parameters(
+      ('unbatched, unbatched value', (4,), (4,), (4,), (4, 2), (4, 2)),
+      ('batched loc, unbatched value', (4,), (7, 4), (4,), (4, 2), (4, 2)),
+      ('batched scale_diag, unbatched value',
+       (4,), (4,), (7, 4), (4, 2), (4, 2)),
+      ('batched scale_u_matrix, unbatched value',
+       (4,), (4,), (4,), (7, 4, 2), (4, 2)),
+      ('batched scale_v_matrix, unbatched value',
+       (4,), (4,), (4,), (4, 2), (7, 4, 2)),
+      ('unbatched, batched value', (7, 4), (4,), (4,), (4, 2), (4, 2)),
+      ('batched loc, batched value', (7, 4), (7, 4), (4,), (4, 2), (4, 2)),
+      ('batched scale_diag, batched value',
+       (7, 4), (4,), (7, 4), (4, 2), (4, 2)),
+      ('batched scale_u_matrix, batched value',
+       (7, 4), (4,), (4,), (7, 4, 2), (4, 2)),
+      ('batched scale_v_matrix, batched value',
+       (7, 4), (4,), (4,), (4, 2), (7, 4, 2)),
+  )
+  def test_log_prob(self, value_shape, loc_shape, scale_diag_shape,
+                    scale_u_matrix_shape, scale_v_matrix_shape):
+    rng = np.random.default_rng(2022)
+    loc = rng.normal(size=loc_shape)
+    scale_diag = rng.normal(size=scale_diag_shape)
+    scale_u_matrix = 0.1 * rng.normal(size=scale_u_matrix_shape)
+    scale_v_matrix = 0.1 * rng.normal(size=scale_v_matrix_shape)
+    dist = MultivariateNormalDiagPlusLowRank(
+        loc=loc,
+        scale_diag=scale_diag,
+        scale_u_matrix=scale_u_matrix,
+        scale_v_matrix=scale_v_matrix,
+    )
+    tfp_dist = tfd.MultivariateNormalFullCovariance(
+        loc=loc,
+        covariance_matrix=_covariance_matrix_from_low_rank(
+            scale_diag, scale_u_matrix, scale_v_matrix)
+    )
+    value = rng.normal(size=value_shape)
+    self.assertion_fn(rtol=2e-3)(
+        self.variant(dist.log_prob)(value), tfp_dist.log_prob(value))
+
+  @chex.all_variants(with_pmap=False)
+  @parameterized.named_parameters(
+      ('unbatched', (4,), (4,), (4, 2), (4, 2)),
+      ('batched loc', (7, 4), (4,), (4, 2), (4, 2)),
+      ('batched scale_diag', (4,), (7, 4), (4, 2), (4, 2)),
+      ('batched scale_u_matrix', (4,), (4,), (7, 4, 2), (4, 2)),
+      ('batched scale_v_matrix', (4,), (4,), (4, 2), (7, 4, 2)),
+      ('scale_u_matrix is None', (4,), (4,), None, None),
+      ('scale_v_matrix is None', (4,), (4,), (4, 2), None),
+  )
+  def test_method(self, loc_shape, scale_diag_shape,
+                  scale_u_matrix_shape, scale_v_matrix_shape):
+    rng = np.random.default_rng(2022)
+    loc = rng.normal(size=loc_shape)
+    scale_diag = rng.normal(size=scale_diag_shape)
+    if scale_u_matrix_shape is None:
+      scale_u_matrix = None
+    else:
+      scale_u_matrix = 0.1 * rng.normal(size=scale_u_matrix_shape)
+    if scale_v_matrix_shape is None:
+      scale_v_matrix = None
+    else:
+      scale_v_matrix = 0.1 * rng.normal(size=scale_v_matrix_shape)
+    dist = MultivariateNormalDiagPlusLowRank(
+        loc=loc,
+        scale_diag=scale_diag,
+        scale_u_matrix=scale_u_matrix,
+        scale_v_matrix=scale_v_matrix,
+    )
+    tfp_dist = tfd.MultivariateNormalFullCovariance(
+        loc=loc,
+        covariance_matrix=_covariance_matrix_from_low_rank(
+            scale_diag, scale_u_matrix, scale_v_matrix)
+    )
+    for method in [
+        'entropy', 'mean', 'stddev', 'variance', 'covariance', 'mode']:
+      if method in ['stddev', 'variance']:
+        rtol = 1e-2
+      elif method in ['covariance']:
+        rtol = 8e-2
+      else:
+        rtol = 1e-3
+      with self.subTest(method=method):
+        fn = self.variant(getattr(dist, method))
+        self.assertion_fn(rtol=rtol)(fn(), getattr(tfp_dist, method)())
+
+  @chex.all_variants(with_pmap=False)
+  @parameterized.named_parameters(
+      ('kl distrax_to_distrax', 'kl_divergence', 'distrax_to_distrax'),
+      ('kl distrax_to_tfp', 'kl_divergence', 'distrax_to_tfp'),
+      ('kl tfp_to_distrax', 'kl_divergence', 'tfp_to_distrax'),
+      ('cross-ent distrax_to_distrax', 'cross_entropy', 'distrax_to_distrax'),
+      ('cross-ent distrax_to_tfp', 'cross_entropy', 'distrax_to_tfp'),
+      ('cross-ent tfp_to_distrax', 'cross_entropy', 'tfp_to_distrax'))
+  def test_with_two_distributions(self, function_string, mode_string):
+    rng = np.random.default_rng(2022)
+    loc1 = rng.normal(size=(5, 1, 4))
+    scale_diag1 = rng.normal(size=(3, 4))
+    scale_u_matrix1 = 0.1 * rng.normal(size=(5, 1, 4, 2))
+    scale_v_matrix1 = 0.1 * rng.normal(size=(5, 1, 4, 2))
+    loc2 = rng.normal(size=(3, 4))
+    scale_diag2 = rng.normal(size=(3, 4))
+    scale_u_matrix2 = 0.1 * rng.normal(size=(4, 2))
+    scale_v_matrix2 = 0.1 * rng.normal(size=(4, 2))
+    distrax_dist1 = MultivariateNormalDiagPlusLowRank(
+        loc=loc1,
+        scale_diag=scale_diag1,
+        scale_u_matrix=scale_u_matrix1,
+        scale_v_matrix=scale_v_matrix1,
+    )
+    distrax_dist2 = MultivariateNormalDiagPlusLowRank(
+        loc=loc2,
+        scale_diag=scale_diag2,
+        scale_u_matrix=scale_u_matrix2,
+        scale_v_matrix=scale_v_matrix2,
+    )
+    tfp_dist1 = tfd.MultivariateNormalFullCovariance(
+        loc=loc1,
+        covariance_matrix=_covariance_matrix_from_low_rank(
+            scale_diag1, scale_u_matrix1, scale_v_matrix1)
+    )
+    tfp_dist2 = tfd.MultivariateNormalFullCovariance(
+        loc=loc2,
+        covariance_matrix=_covariance_matrix_from_low_rank(
+            scale_diag2, scale_u_matrix2, scale_v_matrix2)
+    )
+    expected_result1 = getattr(tfp_dist1, function_string)(tfp_dist2)
+    expected_result2 = getattr(tfp_dist2, function_string)(tfp_dist1)
+    if mode_string == 'distrax_to_distrax':
+      result1 = self.variant(getattr(distrax_dist1, function_string))(
+          distrax_dist2)
+      result2 = self.variant(getattr(distrax_dist2, function_string))(
+          distrax_dist1)
+    elif mode_string == 'distrax_to_tfp':
+      result1 = self.variant(getattr(distrax_dist1, function_string))(tfp_dist2)
+      result2 = self.variant(getattr(distrax_dist2, function_string))(tfp_dist1)
+    elif mode_string == 'tfp_to_distrax':
+      result1 = self.variant(getattr(tfp_dist1, function_string))(distrax_dist2)
+      result2 = self.variant(getattr(tfp_dist2, function_string))(distrax_dist1)
+    self.assertion_fn(rtol=3e-3)(result1, expected_result1)
+    self.assertion_fn(rtol=3e-3)(result2, expected_result2)
+
+  def test_jittable(self):
+    super()._test_jittable(
+        dist_kwargs={'loc': np.zeros((4,))},
+        assertion_fn=self.assertion_fn(rtol=1e-3))
+
+  @parameterized.named_parameters(
+      ('single element', 2),
+      ('range', slice(-1)),
+      ('range_2', (slice(None), slice(-1))),
+  )
+  def test_slice(self, slice_):
+    rng = np.random.default_rng(2022)
+    loc = rng.normal(size=(6, 5, 4))
+    scale_diag = rng.normal(size=(4,))
+    scale_u_matrix = rng.normal(size=(1, 4, 2))
+    scale_v_matrix = rng.normal(size=(4, 2))
+    dist = MultivariateNormalDiagPlusLowRank(
+        loc=loc,
+        scale_diag=scale_diag,
+        scale_u_matrix=scale_u_matrix,
+        scale_v_matrix=scale_v_matrix,
+    )
+    self.assertEqual(dist[slice_].batch_shape, loc[slice_].shape[:-1])
+    self.assertEqual(dist[slice_].event_shape, loc[slice_].shape[-1:])
+    self.assertion_fn(rtol=1e-3)(dist[slice_].mean(), loc[slice_])
+
+  def test_slice_ellipsis(self):
+    rng = np.random.default_rng(2022)
+    loc = rng.normal(size=(6, 5, 4))
+    scale_diag = rng.normal(size=(4,))
+    scale_u_matrix = rng.normal(size=(1, 4, 2))
+    scale_v_matrix = rng.normal(size=(4, 2))
+    dist = MultivariateNormalDiagPlusLowRank(
+        loc=loc,
+        scale_diag=scale_diag,
+        scale_u_matrix=scale_u_matrix,
+        scale_v_matrix=scale_v_matrix,
+    )
+    self.assertEqual(dist[..., -1].batch_shape, (6,))
+    self.assertEqual(dist[..., -1].event_shape, dist.event_shape)
+    self.assertion_fn(rtol=1e-3)(dist[..., -1].mean(), loc[:, -1, :])
+
+
+if __name__ == '__main__':
+  absltest.main()
