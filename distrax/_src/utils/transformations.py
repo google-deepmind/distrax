057d517b29909fb4681588cd3d7b429802eb16ba
Jake Vanderplas <jakevdp@gmail.com>
2026-04-01 16:47:35 -0700

    PR #36092: lax.mul: add out_dtype argument
    Imported from GitHub PR https://github.com/jax-ml/jax/pull/36092
    
    Why? For pallas it would be useful to have a `dot_general`-like `preferred_element_type` argument in the `lax.mul` primitive. This is a quick implementation of this which leaves the default behavior of `lax.mul` unchanged, but allows optionally specifying `preferred_element_type`.
    Copybara import of the project:
    
    --
    00003c2d2182d26972b950fd741f4cb6ce2a0179 by Jake VanderPlas <jakevdp@google.com>:
    
    lax.mul: add out_dtype argument
    
    Merging this change closes #36092
    
    PiperOrigin-RevId: 893177011

--- distrax/_src/utils/transformations.py
+++ distrax/_src/utils/transformations.py
@@ -83,15 +83,17 @@ _inverse_registry = {
     jax.lax.erf_p: jax.lax.erf_inv_p,
     jax.lax.erf_inv_p: jax.lax.erf_p,
     jax.lax.conj_p: jax.lax.conj_p,
-
     # binary ops; tuple values represent the variable-left/variable-right side
     # case for non-commutatively invertible ops like div
-    jax.lax.mul_p: (jax.lax.div_p.bind, lambda x, y: jax.lax.div_p.bind(y, x)),
+    jax.lax.mul_p: (
+        jax.lax.div_p.bind,
+        lambda x, y, **_: jax.lax.div_p.bind(y, x),
+    ),
     jax.lax.div_p: (jax.lax.mul_p.bind, jax.lax.div_p.bind),
     jax.lax.add_p: (jax.lax.sub_p.bind, lambda x, y: jax.lax.sub_p.bind(y, x)),
     jax.lax.sub_p: (jax.lax.add_p.bind, jax.lax.sub_p.bind),
-    jax.lax.pow_p: lambda x, y: jax.lax.pow_p.bind(x, 1.0/y),
-    jax.lax.integer_pow_p: lambda x, y: jax.lax.pow_p.bind(x, 1.0/y)
+    jax.lax.pow_p: lambda x, y: jax.lax.pow_p.bind(x, 1.0 / y),
+    jax.lax.integer_pow_p: lambda x, y: jax.lax.pow_p.bind(x, 1.0 / y),
 }
 
 if hasattr(jax.lax, "square_p"):
